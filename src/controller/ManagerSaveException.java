a2cd5d0754bf7993366f44e7b744fd5477733a85
Allon <anthon.chub@yandex.ru>
2024-08-26 21:21:32 +0300

    sprint7

--- src/controller/ManagerSaveException.java
+++ src/controller/ManagerSaveException.java
@@ -3,7 +3,7 @@ package controller;
 import java.io.IOException;
 
 public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException(final String exp , IOException e) {
+    public ManagerSaveException(final String exp, IOException e) {
         super(exp, e);
     }
 }
